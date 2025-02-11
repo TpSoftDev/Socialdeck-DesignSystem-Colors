39edf4f53a1feee034ee09ef3104d6b2fc28e051
Thabang <tp.softwaredev@gmail.com>
2025-02-11 08:55:14 -0600

    Imported the icon library from figma  and set up a assests folder for the icon and declared them in the pubspec.yaml file might need to re-organize the logos there are too many files to declare in the yaml file

--- socialdeck_ds_colors/lib/main.dart
+++ socialdeck_ds_colors/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'Design_System /colors.dart';
 
 void main() {
@@ -17,6 +18,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Social Deck'),
           backgroundColor: AppColors.warmOffWhite[700],
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 17.0),
+              child: SvgPicture.asset(
+                'assets/icons/Icon/Logo/144.svg',
+                // You might want to adjust the size if needed
+                width: 35,
+                height: 35,
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            SvgPicture.asset('assets/icons/Icon/Logo/36.svg'),
+            SvgPicture.asset('assets/icons/Icon/Logo/144.svg'),
+            SvgPicture.asset('assets/icons/Icon/Add/Button/Large.svg'),
+            SvgPicture.asset('assets/icons/Icon/Apple/24.svg'),
+          ],
         ),
       ),
     );
