48af7388f4ef64c0378aff881c9fbd6afe930736
Thabang <tp.softwaredev@gmail.com>
2025-03-03 12:54:51 -0600

    Refactored the file structure to add light and Dark mode themes

--- socialdeck_ds_colors/lib/main.dart
+++ socialdeck_ds_colors/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'Design_System /colors.dart';
+import 'Design_System /Utils/Themes/colors.dart';
 
 void main() {
   runApp(const MyApp());
